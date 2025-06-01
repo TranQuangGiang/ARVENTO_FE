08d58a2ee53c1cfd47c19ef0e28b00c233a0ca47
caominhhuong <huongcmph52184@gmail.com>
2025-06-01 22:05:42 +0700

    Update Config

--- src/components/client/Register.tsx
+++ src/components/client/Register.tsx
@@ -3,172 +3,127 @@ import { motion, AnimatePresence } from 'framer-motion';
 
 import { useRegister } from "../../hooks/useRegister";
 
-const Register = ({  isOpen, onClose, switchToLogin }: any ) => {
+const Register = ({ isOpen, onClose, switchToLogin }: any) => {
   if (!switchToLogin) return null;
+
   const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
       onClose();
-     
     }
   };
+
   const { mutate } = useRegister({
     resource: "/auth/register",
+  });
 
-  })
-  function onFinish (values:any) {
+  function onFinish(values: any) {
     mutate(values, {
       onSuccess: () => {
         message.success(`Đăng ký tài khoản thành công`);
         switchToLogin();
       },
-      onError: (err:any) => {
+      onError: (err: any) => {
         const errMessage = err?.response?.data?.message || "Đã có lỗi xảy ra ";
-        message.error(errMessage)
-      }
+        message.error(errMessage);
+      },
     });
   }
+
   return (
     <AnimatePresence>
-      {
-        isOpen && (
+      {isOpen && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          className="fixed inset-0 bg-black/80 bg-opacity-40 flex items-center justify-center z-50"
+          onClick={handleOverlayClick}
+          key="overlay"
+        >
           <motion.div
-            initial={{opacity: 0}}
-            animate={{opacity: 1}}
-            exit={{opacity: 0}}
-            transition={{duration: 0.3}}
-            className="fixed inset-0 bg-black/80 bg-opacity-40 flex items-center justify-center z-50"
-            onClick={handleOverlayClick}
-            key="overlay"
+            initial={{ scale: 0.9, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.9, opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="flex flex-col lg:flex-row max-w-5xl min-h-[507px] mx-auto rounded-2xl shadow-lg overflow-hidden bg-white"
+            key="modal"
           >
-            <motion.div
-              initial={{scale: 0.9, opacity: 0}}
-              animate={{scale: 1, opacity: 1}}
-              exit={{scale: 0.9, opacity: 0}}
-              transition={{duration: 0.3}}
-              className="flex flex-col lg:flex-row max-w-5xl min-h-[507px] mx-auto rounded-2xl shadow-lg overflow-hidden bg-white"
-              key="modal"
-            >
-              <div className="w-[50%] text-white lg:w-1/2 p-8 flex flex-col justify-center pt-[0px]">
-                <h2 className="text-3xl font-bold mb-4 text-[#01225a]">Welcome to NIKA</h2>
-                <p className="mb-6 text-[16px] text-[#01225a]">
-                  Create an account to enjoy seamless shopping and exciting offers!
-                </p>
-                <img
-                  src="/images/bangiay1.png"
-                  alt="Shoe"
-                  className="w-[800px] mx-auto"
-                />
-              </div>
+            <div className="w-[50%] text-white lg:w-1/2 p-8 flex flex-col justify-center pt-[0px]">
+              <h2 className="text-3xl font-bold mb-4 text-[#01225a]">Welcome to NIKA</h2>
+              <p className="mb-6 text-[16px] text-[#01225a]">
+                Create an account to enjoy seamless shopping and exciting offers!
+              </p>
+              <img
+                src="/images/bangiay1.png"
+                alt="Shoe"
+                className="w-[800px] mx-auto"
+              />
+            </div>
 
-          <div className="lg:w-1/2 p-8 bg-white">
-            <h3 className="text-2xl font-semibold text-blue-900 mb-6 text-center">
-              Create Your Account
-            </h3>
-            <Form onFinish={onFinish} layout="vertical">
-              <Form.Item 
-                label="Full Name"
-                name="name"
-                rules={[{required: true, min:2, max: 50}]}
-                className="block text-gray-700 font-medium mb-1">
+            <div className="lg:w-1/2 p-8 bg-white">
+              <h3 className="text-2xl font-semibold text-blue-900 mb-6 text-center">
+                Create Your Account
+              </h3>
+              <Form onFinish={onFinish} layout="vertical">
+                <Form.Item
+                  label="Full Name"
+                  name="name"
+                  rules={[{ required: true, min: 2, max: 50 }]}
+                  className="block text-gray-700 font-medium mb-1"
+                >
                   <Input
                     placeholder="John Doe"
                     className="w-full px-4 h-[40px] border border-gray-300 rounded-[4px] focus:outline-1 focus:border-blue-700 outline-none text-[15px]"
                   />
-              </Form.Item>
-              <Form.Item 
-                label="Email"
-                name="email"
-                rules={[{required: true, type: "email"}]}
-                className="block text-gray-700 font-medium mb-1">
+                </Form.Item>
+                <Form.Item
+                  label="Email"
+                  name="email"
+                  rules={[{ required: true, type: "email" }]}
+                  className="block text-gray-700 font-medium mb-1"
+                >
                   <Input
                     placeholder="you@example.com"
                     className="w-full px-4 h-[40px] border border-gray-300 rounded-[4px] focus:outline-1 focus:border-blue-700 outline-none text-[15px]"
                   />
-              </Form.Item>
-              <Form.Item 
-                label="Password"
-                name="password"
-                hasFeedback
-                rules={[{required: true, min:6}]}
-                className="block text-gray-700 font-medium mb-1">
+                </Form.Item>
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  hasFeedback
+                  rules={[{ required: true, min: 6 }]}
+                  className="block text-gray-700 font-medium mb-1"
+                >
                   <Input.Password
                     placeholder="password"
                     className="w-full px-4 h-[40px] border border-gray-300 rounded-[4px] focus:outline-1 focus:border-blue-700 outline-none text-[15px]"
                   />
-              </Form.Item>
-              <div className="lg:w-1/2 p-8 bg-white">
-                <h3 className="text-2xl font-semibold text-blue-900 mb-6 text-center">
-                  Create Your Account
-                </h3>
-                <Form onFinish={onFinish} layout="vertical">
-                  {/* username */}
-                  <div>
-                    <Form.Item 
-                      className="text-gray-700 font-medium mb-1"  
-                      name="name" 
-                      label="Full Name"
-                      rules={[{required: true, min: 2, max: 50}]}
-                    >
-                      <Input
-                        placeholder="John Doe"
-                        className="w-full px-4 h-[40px] text-[15px]"
-                      />
-                    </Form.Item>
-                  </div>
-                  {/* email */}
-                  <div>
-                    <Form.Item 
-                      className="block text-gray-700 font-medium mb-1" 
-                      label="Email" 
-                      name="email"
-                      rules={[{required: true, type: "email"}]}
-                    >
-                      <Input
-                        placeholder="john@example.com"
-                        className="w-full px-4 h-[40px] text-[15px]"
-                      />
-                    </Form.Item>
-                    
-                  </div>
-                  {/* password */}
-                  <div>
-                    <Form.Item 
-                      className="block text-gray-700 font-medium mb-1" 
-                      label="Password"  
-                      name="password"
-                      rules={[{required: true, min: 6}]}
-                      hasFeedback
-                    >
-                      <Input.Password
-                        placeholder="password"
-                        className="w-full px-4 h-[40px] text-[15px]"
-                      />
-                    </Form.Item>
-                  </div>
-                  <Button
-                    style={{height: 45}}
-                    type="primary"
-                    htmlType="submit"
-                    className="w-full mt-[15px] !text-white rounded-[4px] font-semibold hover:bg-blue-800 cursor-pointer transition-all duration-200"
-                  >
-                    Register
-                  </Button>
-                </Form>
+                </Form.Item>
 
-                <p className="text-sm text-center mt-[25px] text-gray-600">
-                  Already have an account?{" "}
-                  <button 
-                    onClick={switchToLogin}
-                    className="text-blue-700 hover:underline cursor-pointer"
-                  >
-                    Log in here
-                  </button>
-                </p>
-              </div>
-            </motion.div>
+                <Button
+                  style={{ height: 45 }}
+                  type="primary"
+                  htmlType="submit"
+                  className="w-full mt-[15px] !text-white rounded-[4px] font-semibold hover:bg-blue-800 cursor-pointer transition-all duration-200"
+                >
+                  Register
+                </Button>
+              </Form>
+
+              <p className="text-sm text-center mt-[25px] text-gray-600">
+                Already have an account?{" "}
+                <button
+                  onClick={switchToLogin}
+                  className="text-blue-700 hover:underline cursor-pointer"
+                >
+                  Log in here
+                </button>
+              </p>
+            </div>
           </motion.div>
-        )
-      }
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 };
--- src/components/contexts/authContexts.tsx
+++ src/components/contexts/authContexts.tsx
@@ -1,7 +1,5 @@
 import { createContext, useEffect, useState } from "react";
 import Cookies from "js-cookie";
-// import { refreshToken } from "../../providers/auth/dataProviders";
-
 
 type AuthContextType = {
   user: any;
@@ -10,15 +8,15 @@ type AuthContextType = {
 };
 
 export const AuthContexts = createContext<AuthContextType>({
-    user: null,
-    login: () => {},
-    logout: () => {}
+  user: null,
+  login: () => {},
+  logout: () => {},
 });
 
-export const AuthProvider = ({ children }:any ) => {
-    const [ user, setUser ] = useState<any>(null);
+export const AuthProvider = ({ children }: any) => {
+  const [user, setUser] = useState<any>(null);
 
-    useEffect(() => {
+  useEffect(() => {
     const storedUser = localStorage.getItem("user");
     const storedToken = localStorage.getItem("token");
 
@@ -26,68 +24,50 @@ export const AuthProvider = ({ children }:any ) => {
     const cookieUser = Cookies.get("user");
 
     if (!storedUser && cookieUser && cookieToken) {
-        try {
-            const parsedUser = JSON.parse(cookieUser);
-            localStorage.setItem("user", cookieUser);
-            localStorage.setItem("token", cookieToken);
-            setUser(parsedUser);
-        } catch (error) {
-            console.error("❌ Lỗi parse cookieUser:", error);
-            Cookies.remove("user");
-            Cookies.remove("token");
-        const storedUser = localStorage.getItem("user");
-        if (storedUser) {
-            const storedToken = localStorage.getItem("token");
-            // đồng bộ lại 
-            const cookieToken = Cookies.get("token");
-            const cookieUser = Cookies.get("user");
-            if (!storedUser && cookieUser && cookieToken) {
-                localStorage.setItem("user", JSON.parse(cookieUser));
-                localStorage.setItem("token", cookieToken);
-                setUser(JSON.parse(cookieUser)); 
-            } else if (storedUser && storedToken) {
-                setUser(JSON.parse(storedUser));
-            }
-        }
+      try {
+        const parsedUser = JSON.parse(cookieUser);
+        localStorage.setItem("user", cookieUser);
+        localStorage.setItem("token", cookieToken);
+        setUser(parsedUser);
+      } catch (error) {
+        console.error("❌ Lỗi parse cookieUser:", error);
+        Cookies.remove("user");
+        Cookies.remove("token");
+      }
     } else if (storedUser && storedToken) {
-        try {
-            setUser(JSON.parse(storedUser));
-        } catch (error) {
-            console.error("❌ Lỗi parse storedUser:", error);
-            localStorage.removeItem("user");
-            localStorage.removeItem("token");
-        }
-    }
-}, []);
-
-
-    const login = (userInfo:any) => {
-        setUser(userInfo);
-        console.log(userInfo);
-
-        {/* Cookies */}
-        Cookies.set("user", JSON.stringify(userInfo.data.user), { expires: 7 });
-        Cookies.set("token", userInfo.data.access_token, { expires: 7 });
-
-        {/* local */}
-        localStorage.setItem("user", JSON.stringify(userInfo.data.user))
-        localStorage.setItem("token", userInfo.data.access_token)
-
-    }
-
-    const logout = () => {
-        setUser(null);
-        // xóa local
+      try {
+        setUser(JSON.parse(storedUser));
+      } catch (error) {
+        console.error("❌ Lỗi parse storedUser:", error);
         localStorage.removeItem("user");
         localStorage.removeItem("token");
-        
-        // xóa cookies
-        Cookies.remove("user");
-        Cookies.remove("token");   
+      }
     }
-    return (
-        <AuthContexts.Provider value={{user, login, logout}}>
-            {children}
-        </AuthContexts.Provider>
-    )
-}
+  }, []);
+
+  const login = (userInfo: any) => {
+    setUser(userInfo);
+
+    // Cookies
+    Cookies.set("user", JSON.stringify(userInfo.data.user), { expires: 7 });
+    Cookies.set("token", userInfo.data.access_token, { expires: 7 });
+
+    // Local storage
+    localStorage.setItem("user", JSON.stringify(userInfo.data.user));
+    localStorage.setItem("token", userInfo.data.access_token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    Cookies.remove("user");
+    Cookies.remove("token");
+  };
+
+  return (
+    <AuthContexts.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContexts.Provider>
+  );
+};
