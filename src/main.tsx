34e2b500f28ebdd7247e848cc02cf58d9424f6ea
TranQuangGiang <giangtqph52177@gmail.com>
2025-06-24 00:04:41 +0700

    add module cart client

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './components/contexts/authContexts.tsx'
+import { CartProvider } from './components/contexts/cartContexts.tsx'
 
 const queryClient = new QueryClient();
 
@@ -13,7 +14,9 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </AuthProvider>
       </QueryClientProvider>
     </BrowserRouter>
